d38eed16d37c48f16605099b637352ca83647cdc
jimblu <jimmy.pezp@gmail.com>
2021-05-21 15:24:31 +0200

    initial commit

---
+++ jamming/src/Components/TrackList/TrackList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Track from '../Track/Track';
+import './TrackList.css';
+
+class TrackList extends React.Component {
+  render() {
+    return (
+      <div className="TrackList">
+        {
+            this.props.tracks.map(track => { 
+                return <Track key={track.id} track={track} onRemove={this.props.onRemove} onAdd={this.props.onAdd} isRemoval={this.props.isRemoval}/>
+            })
+        }
+      </div>
+    );
+  }
+}
+
+
+export default TrackList;
